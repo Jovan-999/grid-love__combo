61886a567389a42bbfef3c22bb35c6814b10677b
Jovan Milinkovic <milinkovicjovanbusiness@gmail.com>
2022-04-21 14:56:44 +0200

    finished_combo

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ['Poppins'],
+      },
+      colors: {
+        primary: '#009cff',
+        secondary: '#61c436',
+        tertiary: '#a66bbe',
+        fourth: '#f99500',
+        fifth: '#d60000',
+      },
+      width: {
+        inherit: 'inherit',
+      },
+      height: {
+        inherit: 'inherit',
+        70: '70px',
+      },
+    },
+  },
+  plugins: [],
+};
